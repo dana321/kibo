73dec3be15c24b78db22f1ee3fe0a692539f7fb3
Dana Adalaide <dana@episteme.ws>
2017-12-31 21:24:24 +0000

    Added symbols and numberpad keys
    * Now has full support for all number pad keys and symbols, apple cmd / windows keys and all other symbol keys. 
    * Changed to use e.which instead of e.keyCode because it is better standardized amongst browsers.

--- kibo.js
+++ kibo.js
@@ -5,7 +5,7 @@ var Kibo = function(element) {
 
 Kibo.KEY_NAMES_BY_CODE = {
   8: 'backspace', 9: 'tab', 13: 'enter',
-  16: 'shift', 17: 'ctrl', 18: 'alt',
+  16: 'shift', 17: 'ctrl', 18: 'alt', 224: 'cmd', 91: 'lcmd', 92:'rwin', 93: 'rcmd',
   20: 'caps_lock',
   27: 'esc',
   32: 'space',
@@ -14,9 +14,15 @@ Kibo.KEY_NAMES_BY_CODE = {
   37: 'left', 38: 'up', 39: 'right', 40: 'down',
   45: 'insert', 46: 'delete',
   48: '0', 49: '1', 50: '2', 51: '3', 52: '4', 53: '5', 54: '6', 55: '7', 56: '8', 57: '9',
+  59: 'semi_colon',61: 'equals_sign',
   65: 'a', 66: 'b', 67: 'c', 68: 'd', 69: 'e', 70: 'f', 71: 'g', 72: 'h', 73: 'i', 74: 'j', 75: 'k', 76: 'l', 77: 'm', 78: 'n', 79: 'o', 80: 'p', 81: 'q', 82: 'r', 83: 's', 84: 't', 85: 'u', 86: 'v', 87: 'w', 88: 'x', 89: 'y', 90: 'z',
+  96: '0', 97: '1', 98: '2', 99: '3', 100: '4', 101: '5', 102: '6', 103: '7', 104: '8', 105: '9',
+  106:'multiply',107:'add',109:'subtract',110: 'decimal_point',111:'divide',
   112: 'f1', 113: 'f2', 114: 'f3', 115: 'f4', 116: 'f5', 117: 'f6', 118: 'f7', 119: 'f8', 120: 'f9', 121: 'f10', 122: 'f11', 123: 'f12',
-  144: 'num_lock'
+  144: 'num_lock', 145: 'scroll_lock', 
+  173: 'dash',
+  186: 'semi_colon', 187: 'equal_sign', 188: 'comma', 189: 'dash', 190: 'period', 191: 'forward_slash',192: 'grave_accent',
+  219: 'open_bracket',220:'back_slash',221:'close_bracket',222:'single_quote',
 };
 
 Kibo.KEY_CODES_BY_NAME = {};
@@ -26,15 +32,16 @@ Kibo.KEY_CODES_BY_NAME = {};
       Kibo.KEY_CODES_BY_NAME[Kibo.KEY_NAMES_BY_CODE[key]] = +key;
 })();
 
-Kibo.MODIFIERS = ['shift', 'ctrl', 'alt'];
+Kibo.MODIFIERS = ['shift', 'ctrl', 'alt', 'cmd','lcmd','rcmd','rwin'];
 
-Kibo.WILDCARD_TYPES = ['arrow', 'number', 'letter', 'f'];
+Kibo.WILDCARD_TYPES = ['arrow', 'number', 'letter', 'f', 'symbol'];
 
 Kibo.WILDCARDS = {
   arrow: [37, 38, 39, 40],
-  number: [48, 49, 50, 51, 52, 53, 54, 55, 56, 57],
+  number: [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105],
   letter: [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90],
-  f: [112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123]
+  f: [112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123],
+  symbol: [59,61,106,107,109,109,110,111,173,186,187,188,189,190,191,192,219,220,221,222]
 };
 
 Kibo.registerEvent = (function() {
@@ -170,7 +177,7 @@ Kibo.prototype.handler = function(upOrDown) {
 
     e = e || window.event;
 
-    that.lastKeyCode = e.keyCode;
+    that.lastKeyCode = e.which;
     for(i = 0; i < Kibo.MODIFIERS.length; i++)
       that.lastModifiers[Kibo.MODIFIERS[i]] = e[Kibo.MODIFIERS[i] + 'Key'];
     if(Kibo.arrayIncludes(Kibo.MODIFIERS, Kibo.keyName(that.lastKeyCode)))
